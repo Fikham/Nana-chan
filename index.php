36d982a082a6df2d0f05c4655e156f8dee0c745d
Fikham <42429176+Fikham@users.noreply.github.com>
2018-08-20 22:18:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'YOUR-API-KEY-SIMSIMI'; //API SimSimi
+$key = '1145aafa-2817-45f6-a44a-5f6fb7418c9d'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
